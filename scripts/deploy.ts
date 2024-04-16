d3cc574edf950c7729d9140e48334ad73111693b
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-04-16 12:35:05 +0100

    Contract Deployed

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from 'hardhat';
+
+async function main() {
+  const nft = await ethers.deployContract('NFT');
+
+  await nft.waitForDeployment();
+
+  console.log('NFT Contract Deployed at ' + nft.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
